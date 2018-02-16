c05655099194b97bb97a3428efad6b6fed73f96b
shivjoshi1996 <shivam.joshi@canonical.com>
2018-02-16 15:12:43 +0000

    Add version number to main script

--- updateCurrentIteration.js
+++ updateCurrentIteration.js
@@ -1,30 +1,6 @@
-// Edit here
-var EPIC_NAME_COLUMN = 'A';
-var EPIC_LINK_COLUMN = 'E';
-var TOTAL_COLUMN = 'F';
-var DONE_COLUMN = 'G';
-var STATUS_COLUMN = 'B';
-var START_OF_ITERATION_STRING = 'current';
-var END_OF_ITERATION_STRING = 'next';
-var ACCESS_TOKEN = ''; // Required, ask someone.
-var RATE_LIMIT = 606;
-
-/*
- * Object of repoIds:
- * {
- *   'ORG/REPO': '123',
- *   'ORG/REPO2': '321',
- *   'ORG/REPO3': '213'
- * }
-*/
-var REPO_IDS = {};
-// Stop editing
-
-var baseUrl = 'https://api.zenhub.io';
-var apiToken = '?access_token=' + ACCESS_TOKEN;
-
-var startOfIteration = 1;
-var endOfIteration = 2;
+// ZenHub backlog updater
+// Version: v0.0.1
+// https://github.com/canonical-webteam/zenhub-backlog-updater/
 
 function onOpen() {
   var spreadsheet = SpreadsheetApp.getActive();
